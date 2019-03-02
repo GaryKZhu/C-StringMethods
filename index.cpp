cb13edf11bd5f4e011fdaf45d4ef9a2eda4e671c
GaryKZhu <39391898+GaryKZhu@users.noreply.github.com>
2019-03-02 13:10:49 -0500

    Create index.cpp

---
+++ index.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <sstream>
+#include <algorithm>"
+
+using namespace std;
+int main(){
+    string inputstr = "I like Evan";
+    string x;
+    
+    x = inputstr.substr(2,4);
+    
+    
+    cout << x;
+
+}
